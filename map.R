894cb91faec515d24251eeb00e1f450f5963555b
mikejt33 <mikejt33@gmail.com>
2018-05-13 16:40:40 -0400

    map

--- map.R
+++ map.R
@@ -10,7 +10,7 @@ data <- read.csv("emails.country.csv")
 
 #data$Name[which(data$Name=="Russian Federation")]<-"Russia"
 
-
+ 
 #data$Name <- as.character(data$Name)
 #data$Name[data$Name == "Russian Federation"] <- "Russia"
 
@@ -24,13 +24,21 @@ data5 <- data.frame(count(data$region))
 setnames(data5, "x", "region")
 
 
+
 map1 <- left_join(map, data5, by = "region")
 map1$freq %>% replace_na(0)
 
+map2<- map1 %>%
+  mutate(
+    fake = freq
+  )
+
 p <- ggplot() 
-p <- p + geom_polygon(data=map1, aes(x=long, y=lat, group = group, text = region, fill=log(freq)),colour="white"
-) + scale_fill_continuous(low = "lightgreen", high = "darkgreen", guide="colorbar")
+p <- p + geom_polygon(data=map2, aes(x=long, y=lat, group = group, text = paste("Country: ",region,"\n","Count: ",fake) ,fill=log(freq)),colour="white"
+) + scale_fill_continuous(low = "lightgreen", high = "darkgreen", guide="colorbar") +
+  labs(title= "Emails Associated with Publications") + theme_minimal()
 
-
-p <- ggplotly(p)
+p <- ggplotly(p, tooltip = "text")
 p
+
+htmlwidgets::saveWidget(p, "map.html")
